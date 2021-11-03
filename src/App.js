96142c0de192ed3af4573e3beafd31649dddb7d4
Oktado Putra <oktado.putra@gmail.com>
2021-11-03 10:02:22 +0700

    3rd

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Test React
+          Test 123 React
         </a>
       </header>
     </div>
